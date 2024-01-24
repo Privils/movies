c626e54ee8f9ef590e064ad4720a133babd40c62
Priviledge <privilledgeblack8@gmail.com>
2024-01-24 13:53:02 +0200

    still in progress

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
+const api_key = "api_key=bd2c4a5cdba2e9ed745b08b51d624d57"
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + '/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&' + api_key;
+const image_url = 'https://image.tmdb.org/t/p/w500/';
+const main = document.getElementById("main");
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const search_url = BASE_URL + '/search/movie?' + api_key;
+getMovie(API_URL);
 
-const API_KEY = "api_key=2396da8117cda1bcdd20fa75f49591f6";
-const BASE_URL ="https://api.themoviedb.org/3";
-const API_URL = BASE_URL + '/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&' + API_KEY
-const image_url = 'https://image.tmdb.org/t/p/w500/'
-const main = document.getElementById("main")
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const search_url = BASE_URL + '/search/movie?' + API_KEY;
+function getMovie(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            showMovies(data.results);
+        });
+}
 
-getMovie(API_URL)
-
-function getMovie(url){
-fetch(url).then(res => res.json())
-.then(data => { 
-
-    showMovies(data.results);
-})
-
-function showMovies(data){
-   main.innerHTML = '';
+function showMovies(data) {
+    main.innerHTML = '';
 
     data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie
+        const { title, poster_path, vote_average, overview } = movie;
         const movieEl = document.createElement('div');
-        movieEl.classList.add('movie')
+        movieEl.classList.add('movie');
         movieEl.innerHTML = `
-        <img src="${image_url+poster_path}" alt="${title}"/>
+        <img src="${image_url + poster_path}" alt="${title}"/>
         <div class="movie-info">
             <h2>${title}</h2>
             <span class="${getColor(vote_average)}">${vote_average}</span>
@@ -33,30 +32,30 @@ function showMovies(data){
         <div class="overview">
             <h1>Overview</h1>
             ${overview}
-        </div>`
+        </div>`;
 
-        main.appendChild(movieEl)
-    })
+        main.appendChild(movieEl);
+    });
 }
-}
-function getColor(vote){
-    if(vote>=8){
-        return 'green'
-    } else if(vote >= 5){
-        return 'orange'
-    } else{
-        return 'red'
+
+function getColor(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
     }
-        
 }
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
     const searchTerm = search.value;
 
-    if(searchTerm){
-        getMovie(search_url+'&query='+searchTerm)
-    } else{
-        getMovie(API_URL)
+    if (searchTerm) {
+        getMovie(search_url + '&query=' + searchTerm);
+    } else {
+        getMovie(API_URL);
     }
 })
